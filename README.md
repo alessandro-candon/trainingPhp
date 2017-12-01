639469042a12c22ad7411c0908a2e2359b1b5483
Alessandro Candon <alessandro.candon@oxylane.com>
2017-12-01 21:05:03 +0100

    add override

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ In order to use this project run
 ``composer install``
 
 and then run tests
+
+
--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -9,12 +9,37 @@ use src\Calculator;
 
 class CalculatorTest extends TestCase
 {
+    public static function setUpBeforeClass()
+    {
+        echo "setUpBeforeClass \n";
+    }
+
+    public function setUp()
+    {
+        echo "setUp \n";
+    }
+
+    public function tearDown()
+    {
+        echo "tearDown \n";
+    }
+
+    public static function tearDownAfterClass()
+    {
+        echo "tearDownAfterClass \n";
+    }
+
     public function test_when_sum_given_one_one_then_two()
     {
         $calc = new Calculator();
         $response = $calc->sum(1, 1);
 
         $this->assertEquals(2, $response);
+    }
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
     }
 
 }
